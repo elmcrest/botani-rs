40a1ff3fc269a2ea245d74331b00a4fccb3a7366
Marius Räsener <931919+elmcrest@users.noreply.github.com>
2024-02-29 10:24:49 +0100

    compiler error trait Pin not implemented for PIO0

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,55 @@
 #![no_std]
 #![no_main]
-#![feature(type_alias_impl_trait)]
 
+use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::pio::{InterruptHandler, Pio};
 use embassy_time::{Duration, Timer};
-use gpio::{Level, Output};
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+#[embassy_executor::task]
+async fn wifi_task(
+    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+) -> ! {
+    runner.run().await
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     // Initialise Peripherals
     let p = embassy_rp::init(Default::default());
+    let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
 
-    // Create LED
-    let mut led = Output::new(p.PIN_25, Level::Low);
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
 
-    // Loop
-    loop {
-        // Log
-        info!("LED On!");
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(wifi_task(runner)));
 
-        // Turn LED On
-        led.set_high();
-
-        // Wait 100ms
-        Timer::after(Duration::from_millis(100)).await;
-
-        // Log
-        info!("LED Off!");
-
-        // Turn Led Off
-        led.set_low();
-
-        // Wait 100ms
-        Timer::after(Duration::from_millis(100)).await;
-    }
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
 }
